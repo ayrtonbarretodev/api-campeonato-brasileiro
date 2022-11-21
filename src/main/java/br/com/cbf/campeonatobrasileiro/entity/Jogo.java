7aa8b819f5ca05e9ce949b520a2fd771c2546275
Ayrton <devayrtonsbarreto@gmail.com>
2022-11-21 11:25:23 -0300

    Criado os relacionamentos entre a entidade Jogo e Time

--- src/main/java/br/com/cbf/campeonatobrasileiro/entity/Jogo.java
+++ src/main/java/br/com/cbf/campeonatobrasileiro/entity/Jogo.java
@@ -2,18 +2,19 @@ package br.com.cbf.campeonatobrasileiro.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
-//@Entity
+@Entity
 public class Jogo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "timeMandante")
     private Time timeMandante;
+    @ManyToOne
+    @JoinColumn(name = "timeVisitante")
     private Time timeVisitante;
     private Integer golsTimeMandante;
     private Integer golsTimeVisitante;
