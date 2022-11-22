be2ffe42a5b8ec1f07fd0cf947f268abfbd67ecd
Ayrton <devayrtonsbarreto@gmail.com>
2022-11-21 21:30:58 -0300

    Criado rest com os métodos para buscar time, buscar todos os times e salvar um time

---
+++ src/main/java/br/com/cbf/campeonatobrasileiro/rest/TimeRestController.java
@@ -0,0 +1,53 @@
+package br.com.cbf.campeonatobrasileiro.rest;
+
+import br.com.cbf.campeonatobrasileiro.dto.TimeDTO;
+import br.com.cbf.campeonatobrasileiro.entity.Time;
+import br.com.cbf.campeonatobrasileiro.service.TimeServico;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/times")
+public class TimeRestController {
+
+    @Autowired
+    private TimeServico timeServico;
+
+    @Operation(summary = "Lista todos os times",description = "Listando todas os times",responses = {
+            @ApiResponse(responseCode = "200", description = "Times listados com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Houve um erro ao listar os times")
+    })
+
+    @GetMapping()
+    public ResponseEntity<List<Time>> getTimes(){
+        return ResponseEntity.ok().body(timeServico.listarTimes());
+    }
+
+    @Operation(summary = "Obtem os dados de um time",description = "Buscando uma tarefa por id", responses = {
+            @ApiResponse(responseCode = "200", description = "Time encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrado nenhum time com esse id")
+    })
+
+    @GetMapping("{id}")
+    public ResponseEntity<Time> getTime(@PathVariable(value = "id") Integer id){
+        Time time = timeServico.buscarTime(id);
+        return ResponseEntity.ok().body(time);
+    }
+
+    @Operation(summary = "Criar Time",description = "Criando um novo time",responses = {
+            @ApiResponse(responseCode = "201", description = "Time criado com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Houve um erro ao criar o time, verifique as informações")
+    })
+
+    @PostMapping()
+    public ResponseEntity<Time> createTime(@RequestBody TimeDTO timeDTO) {
+        Time time = timeDTO.toTime();
+        return ResponseEntity.ok().body(timeServico.cadastrarTime(time));
+    }
+}
