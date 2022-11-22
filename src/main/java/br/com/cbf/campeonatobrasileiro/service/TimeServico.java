8ac690434bc4eb20d382641002e7090078b8a85c
Ayrton <devayrtonsbarreto@gmail.com>
2022-11-21 21:35:13 -0300

    Removido importas não utilizados

--- src/main/java/br/com/cbf/campeonatobrasileiro/service/TimeServico.java
+++ src/main/java/br/com/cbf/campeonatobrasileiro/service/TimeServico.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TimeServico {
@@ -25,4 +24,6 @@ public class TimeServico {
     public Time buscarTime(Integer id){
         return timeRepository.findById(id).get();
     }
+
+
 }
