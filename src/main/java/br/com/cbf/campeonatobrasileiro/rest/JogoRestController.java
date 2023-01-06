cf95fc0aedeb9c22fe9e9561c27b4afd5e3a974a
Ayrton <devayrtonsbarreto@gmail.com>
2023-01-06 14:11:31 -0300

    Criado os end points para finalizar uma partida, obter a quantidade de rodadas, obter todos os jogos e obter jogos de um time específico e buscar determinado jogo

--- src/main/java/br/com/cbf/campeonatobrasileiro/rest/JogoRestController.java
+++ src/main/java/br/com/cbf/campeonatobrasileiro/rest/JogoRestController.java
@@ -1,17 +1,18 @@
 package br.com.cbf.campeonatobrasileiro.rest;
 
+import br.com.cbf.campeonatobrasileiro.dto.ClassificacaoDTO;
 import br.com.cbf.campeonatobrasileiro.dto.JogoDTO;
+import br.com.cbf.campeonatobrasileiro.dto.JogoFinalizadoDTO;
 import br.com.cbf.campeonatobrasileiro.entity.Jogo;
 import br.com.cbf.campeonatobrasileiro.service.JogoServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jogos")
@@ -27,6 +28,42 @@ public class JogoRestController {
 
     @GetMapping
     public ResponseEntity<List<JogoDTO>> obterJogos(){
-        return ResponseEntity.ok().body(jogoServico.obterJogos());
+        return ResponseEntity.ok().body(jogoServico.listarJogos());
+    }
+
+    @GetMapping("/time/{time}")
+    public ResponseEntity<List<JogoDTO>> obterTodosJogosDeUmTime(@PathVariable(value = "time") String time){
+        return ResponseEntity.ok().body(jogoServico.obterTodosJogosDeUmTime(time));
+    }
+
+    @GetMapping("quantidade-rodadas")
+    public ResponseEntity<Long> obterQuantidadeDeRodadas(){
+        return ResponseEntity.ok().body(jogoServico.quantidadeRodadas());
+    }
+
+    @PostMapping("/finalizar/{id}")
+    public ResponseEntity<JogoDTO> finalizar(@PathVariable Integer id, @RequestBody JogoFinalizadoDTO jogoFinalizadoDto) throws Exception {
+        return ResponseEntity.ok().body(jogoServico.finalizar(id,jogoFinalizadoDto));
+    }
+
+//    @PostMapping("/finalizar/{id}")
+//    public ResponseEntity<JogoDTO> finalizarJogo(@PathVariable Integer id, @RequestBody JogoDTO jogoDto) throws Exception {
+//        Optional<Jogo> jogo = jogoServico.buscarJogoPorId(id);
+//
+//        if (!jogo.isPresent()){
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(throw new Exception(););
+//        }
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(jogoServico.finalizarJogo(id,jogoDto));
+//    }
+
+//    @GetMapping("/classificacao")
+//    public ResponseEntity<List<ClassificacaoDTO>> obterClassificacao(){
+//        return ResponseEntity.ok().body(jogoServico.obterClassificacao());
+//    }
+
+    @GetMapping("/jogo/{id}")
+    public ResponseEntity<JogoDTO> buscarJogo(@PathVariable (value = "id") Integer id){
+        return ResponseEntity.ok().body(jogoServico.buscarJogo(id));
     }
 }
