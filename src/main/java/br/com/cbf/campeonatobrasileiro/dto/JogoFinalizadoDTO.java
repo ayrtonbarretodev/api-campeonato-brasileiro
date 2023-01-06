a4bb9e0e08fc98f593c83a09e156729be2477ffe
Ayrton <devayrtonsbarreto@gmail.com>
2023-01-06 14:12:21 -0300

    Criado os dtos para representar os dados do JogoFinalizado e Classificacao

---
+++ src/main/java/br/com/cbf/campeonatobrasileiro/dto/JogoFinalizadoDTO.java
@@ -0,0 +1,10 @@
+package br.com.cbf.campeonatobrasileiro.dto;
+
+import lombok.Data;
+
+@Data
+public class JogoFinalizadoDTO {
+    private Integer golsTimeMandante;
+    private Integer golsTimeVisitante;
+    private Integer publicoPagante;
+}
